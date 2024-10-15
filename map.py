29e880722965fc7dea4546af29d2a17a03a214dd
rhysd <rhysdbjones06@gmail.com>
2024-10-15 15:45:49 +0100

    Adding functions to the map and getting rid of all the comments from the map

--- map.py
+++ map.py
@@ -1,5 +1,5 @@
 from items import *
-
+global starting_position
 map_matrix = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -10,85 +10,17 @@ map_matrix = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
-
-
-room_reception = {
-    "name": "Reception",
-
-    "description":
-    """You are in a maze of twisty little passages, all alike.
-Next to you is the School of Computer Science and
-Informatics reception. The receptionist, Matt Strangis,
-seems to be playing an old school text-based adventure
-game on his computer. There are corridors leading to the
-south and east. The exit is to the west.""",
-
-    "exits": {"south": "Admins", "east": "Tutor", "west": "Parking"},
-
-    "items": [item_biscuits, item_handbook]
-
-}
-
-room_admins = {
-    "name": "MJ and Simon's room",
-
-    "description":
-    """You are leaning agains the door of the systems managers'
-room. Inside you notice Matt "MJ" John and Simon Jones. They
-ignore you. To the north is the reception.""",
-
-    "exits":  {"north": "Reception"},
-
-    "items": []
-}
-
-room_tutor = {
-    "name": "your personal tutor's office",
-
-    "description":
-    """You are in your personal tutor's office. He intently
-stares at his huge monitor, ignoring you completely.
-On the desk you notice a cup of coffee and an empty
-pack of biscuits. The reception is to the west.""",
-
-    "exits": {"west": "Reception"},
-
-    "items": []
-}
-
-room_parking = {
-    "name": "the parking lot",
-
-    "description":
-    """You are standing in the Queen's Buildings parking lot.
-You can go south to the COMSC reception, or east to the
-general office.""",
-
-    "exits": {"east": "Office", "south": "Reception"},
-
-    "items": []
-}
-
-room_office = {
-    "name": "the general office",
-
-    "description":
-    """You are standing next to the cashier's till at
-30-36 Newport Road. The cashier looks at you with hope
-in their eyes. If you go west you can return to the
-Queen's Buildings.""",
-
-    "exits": {"west": "Parking"},
-
-    "items": [item_pen]
-}
+#This is the matrix which will be filled with the random unique numbers that are going to be used to place the rooms on the map
 
 
 
-rooms = {
-    "Reception": room_reception,
-    "Admins": room_admins,
-    "Tutor": room_tutor,
-    "Parking": room_parking,
-    "Office": room_office
-}
+def door_assigner(room_num): #This function assigns the random door directions to each of the numbered doors stored in the matrix.
+    pass
+def where_am_i(x, y): #This function takes the coordinates of the player and returns the room that the player is in and calculates how many positions from the edge the player is using north, south, east and west.
+    return map_matrix[x][y]
+def move(x, y, direction): #This function takes the coordinates of the player and the direction that the player wants to move in and returns the new coordinates of the player after the move.
+    pass
+
+
+
+
