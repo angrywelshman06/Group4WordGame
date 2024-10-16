f394017fb07ef2e89ae514391137cecaea11c9ff
Sam Tyler <samtyler905@gmail.com>
2024-10-16 16:02:27 +0100

    Added dist_from_edge function

--- map.py
+++ map.py
@@ -50,16 +50,24 @@ def generate_map():
 
 def door_assigner(room_num): #This function assigns the random door directions to each of the numbered doors stored in the matrix.
     pass
-def where_am_i(x, y): #This function takes the coordinates of the player and returns the room that the player is in and calculates how many positions from the edge the player is using north, south, east and west.
-    return map_matrix[x][y]
+
 def move(x, y, direction): #This function takes the coordinates of the player and the direction that the player wants to move in and returns the new coordinates of the player after the move.
     pass #This function will be used to move the player around the map based on the direction inputted by the user
+
 def dist_from_start(x, y): #This function takes the coordinates of the player and returns the distance from the starting position of the player
     starting = starting_position
     distance = abs(x - starting[0]) + abs(y - starting[1])
     return distance
 
+# Returns a dictionary of the distances for each edge from the player
+def dist_from_edge(x, y) -> {}:
+    return {
+        "north" : y,
+        "east" : len(map_matrix[y]) - x,
+        "south" : len(map_matrix) - y,
+        "west" : x
+    }
 
-generate_map()
-print(map_matrix)
-
+# Gets the room based off its matrix position coordinates
+def get_room(x, y) -> Room:
+    return map_matrix[y][x]
