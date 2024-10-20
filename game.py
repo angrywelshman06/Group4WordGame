9643957297a010faedf5137c1edb8f9dad80714f
Sam Tyler <samtyler905@gmail.com>
2024-10-20 01:13:16 +0100

    Changed execute_go to work with sets

--- game.py
+++ game.py
@@ -46,7 +46,7 @@ def is_valid_exit(direction):
 
 def execute_go(direction):
     if is_valid_exit(direction):
-        new_pos = player.current_room_position[:]
+        new_pos = player.current_room_position
 
         # Translating direction into vector movement
         match direction:
@@ -55,27 +55,19 @@ def execute_go(direction):
             case "south": new_pos[1] += 1
             case "west": new_pos[0] -= 1
 
-        current_room = player.get_current_room()
-        new_room = get_room(new_pos[0], new_pos[1])
-
-        if new_room is None:
-            new_room = Room()
-            map_matrix[new_pos[1]][new_pos[0]] = new_room
-
-            # Generate doors for the new room
-            new_room.exits = door_assigner(len(map_matrix), len(map_matrix[0]), new_pos[0], new_pos[1])
-
-            # Ensure the new room has an exit back to the current room
-            opposite_direction = {"north": "south", "south": "north", "east": "west", "west": "east"}
-            new_room.exits[opposite_direction[direction]] = current_room
-
-        # Update the current room's exits
-        current_room.exits[direction] = new_room
-
+        # Update previous and current room positions
         player.previous_room_position = player.current_room_position
         player.current_room_position = new_pos
 
-        print(f"You are going to {new_room.name}.")
+        # Generate doors for new room
+        if not len(player.get_current_room().exits): # If exits have not been generated
+            player.get_current_room().exits = door_assigner(len(map_matrix), len(map_matrix[0]), new_pos[0], new_pos[1])
+
+        # Ensure the new room has an exit back to the current room
+        opposite_direction = {"north": "south", "south": "north", "east": "west", "west": "east"}
+        player.get_current_room().exits.add(opposite_direction[direction])
+
+        print(f"You are going to {player.get_current_room().name}.")
     else:
         print("You cannot go there.")
 
@@ -150,7 +142,6 @@ def main():
 
     # Startup Logic
     generate_map()
-
 
     # Main game loop
     while True:
