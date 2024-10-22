6c33ef2e98c428920c8c4cc8a8a36681eda598eb
Sam Tyler <samtyler905@gmail.com>
2024-10-22 22:32:36 +0100

    Minor changes

--- enemies.py
+++ enemies.py
@@ -1,14 +1,15 @@
 class Enemy:
-    def __init__(self, enemy : {}):
+    def __init__(self, enemy : {}, level=1):
         self.name = enemy["name"]
         self.description = enemy["description"]
+        self.level = level
         self.health = enemy["health"]
-        self.damage = enemy["damage"]
+        self.damage = enemy["base_damage"] * (1 + 0.4 * self.level)
 
 
 zombie = {
     "name" : "zombie",
     "description" : "DESCRIPTION",
     "health" : 50,
-    "damage" : 10
+    "base_damage" : 10
 }
