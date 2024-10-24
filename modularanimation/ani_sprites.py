7d275311969eaf55e474beacf31d75933b922c99
alxm9 <alxmoc96@gmail.com>
2024-10-24 14:47:42 +0100

    updated combat

---
+++ modularanimation/ani_sprites.py
@@ -0,0 +1,179 @@
+import os
+import curses
+
+# Here so I can set colors
+curses.initscr()
+
+# Run this in the mainloop of the main file
+def curses_setcolors():
+    curses.init_pair(1, 0, 243), # GREY
+    curses.init_pair(2, 0, 136), # WOOD MATERIALS 1
+    curses.init_pair(3, 0, 240), # DARK ROOM
+    curses.init_pair(4, 0, 34), # GREEN 1
+    curses.init_pair(5, 0, 64), # GREY 2 
+    curses.init_pair(6, 178, 88), # FIRE
+    curses.init_pair(7, 70, 0), # ZOMBIE GREEN
+    curses.init_pair(8, 196, 52), # RED
+    curses.init_pair(9, 245, 0), # HELICOPTER GREY
+    curses.init_pair(10, 0, 249), # BUILDINGS GREY
+    curses.init_pair(11, 0, 33), # PROTAG_BLUE
+    curses.init_pair(12, 15, 0), # PILLOW WHITE
+    curses.init_pair(13, 33, 33), # PROTAG_BLUE2
+    curses.init_pair(14, 178,33), # ENEMY INFO PAIR
+    
+class spritesheet():
+    def __init__(self, path, color, zlevel = 0, frames = 0, dx = 0, dy = 0, delay = 0, infolist = 0):
+        self.path = path
+        self.color = color # curses pair
+        self.zlevel = zlevel
+        self.frames = frames
+        self.dx = dx 
+        self.dy = dy
+        self.delay = delay # not implemented
+        self.frameslist = self.gather_framelist() # list containing every frame
+        self.infolist = infolist
+        if infolist != 0:
+            self.frameslist = self.transform_to_details()
+        if (self.dx != 0) or (self.dy != 0):
+            self.frameslist = self.extend_frames()
+
+    def gather_framelist(self):
+        frameslist = []
+        for frame in range(1,self.frames+1):
+            # path = os.path.join("modularanimation","spritesheets","{0}".format(self.path), "txt", "ascii-art ({0}).txt".format(frame)) ## os.path.join works regardless of OS
+            path = os.path.join("spritesheets",*self.path, "ascii-art ({0}).txt".format(frame)) #if ran directly from animator.py
+            file = open(path, "r", encoding="utf-8")
+            frameslist.append(file.read())
+            file.close()
+        return frameslist
+
+    def transform_to_details(self): # By now we have a creature_dict value : spritesheet pair in info_dict
+        name = self.infolist[0]
+        number = self.infolist[1]
+        level = self.infolist[2]
+        hp = self.infolist[3]
+        newframeslist = []
+        
+        for index,value in enumerate(self.frameslist):
+            newframe = self.frameslist[index].splitlines()     
+            
+            for index, value in enumerate(newframe):
+                newframe[index] = " "*len(newframe[index])
+            # for line in newframe:
+                # line = " "*len(line)
+            
+            for index,line in enumerate(newframe):
+                if index == 2:
+                    newframe[index] = name +" No."+str(number)+line[len(name+" No."+str(number)):]
+                if index == 3:
+                    newframe[index] = "Lv"+str(level)+" HP:"+str(hp)+line[index][len("Lv"+str(level)+" HP:"+str(hp)):]
+                else:
+                    pass
+            # for line in newframe:
+                # for index, char in enumerate(line):
+                    # if index == 2:
+                        # line[index] = name +" No."+str(number)+line[index][len(name+" No."+str(number)):]
+                    # if index == 3:
+                        # line[index] = "Lv"+str(level)+" HP:"+str(hp)+line[len("Lv"+str(level)+" HP:"+str(hp)):]
+                    # else:
+             
+            
+            newframe = "\n".join(newframe) # joins back the frames
+            newframeslist.append(newframe)
+        
+        return newframeslist
+                
+                   
+                
+    def extend_frames(self):
+        ### Standard: col 100, rows 34
+        # don't go out of the standard bounds when working with dx and dy
+        newframeslist = []
+        for index, value in enumerate(self.frameslist):
+            newframe = self.frameslist[index].splitlines()
+            
+            for index, value in enumerate(newframe):
+                if len(newframe[index]) < 100:
+                    newframe[index] += " "*(100-len(newframe[index])) 
+                    
+            if len(newframe) < 34: # add empty space 
+                for i in range(len(newframe), 34):
+                    newframe.append(" "*100)
+                    
+            for i in range(self.dy): # DY adjustment
+                newframe.insert(0,(" "*100))
+                del newframe[-1]
+                
+            if self.dx != 0:
+                for index, value in enumerate(newframe): # DX adjustment
+                    newframe[index] = (" "*(self.dx) + newframe[index])[:-(self.dx)]
+                    
+            newframe = "\n".join(newframe) # joins back the frames
+            newframeslist.append(newframe)
+            
+        return newframeslist
+        
+# All txt frames should have the same amount of lines and same col at the end of a line. For now.
+##################### path, escape code color, z-level, frames
+
+###### Test
+# outline = spritesheet(("bg",),"\033[37m", zlevel = 22, frames = 1)
+# circle = spritesheet(("circle",),"\033[94m", zlevel = 6, frames = 5)
+# square = spritesheet(("square",),"\033[32m", zlevel = 4, frames = 5)
+# lightning = spritesheet(("lightning",),"\033[32m", zlevel = 5, frames = 5)
+# dude = spritesheet(("dude",), "\033[32m", zlevel= 5, frames = 28)
+# frontpillars = spritesheet(("frontpillars",),"\033[31m", zlevel = 8, frames = 28)
+# backpillars = spritesheet(("backpillars",),"\033[31m", zlevel = 2, frames = 28)
+
+###### Multi-purpose
+general_bg = spritesheet(("background",), 3, zlevel = 0.05, frames = 1)
+outline = spritesheet(("outline1",), 9, zlevel = 77, frames = 1)
+randomobj = spritesheet(("randomobj",), 8, zlevel = 134, frames = 1, dx = 55, dy = 10)
+
+###### Intro part 1
+building1 = spritesheet(("intro_1","building1"), 10, zlevel = 7, frames = 95)
+heli = spritesheet(("intro_1","heli"), 9, zlevel = 5, frames = 95)
+skyscraper = spritesheet(("intro_1","skyscraper"), 10, zlevel = 4, frames = 95)
+fire3 = spritesheet(("intro_1","fire3"), 6, zlevel = 3, frames = 95)
+fire2 = spritesheet(("intro_1","fire2"), 6, zlevel = 8, frames = 95)
+fire = spritesheet(("intro_1","fire"), 6, zlevel = 6, frames = 95)
+citybg = spritesheet(("intro_1","citybg"), 10, zlevel = 2, frames = 95)
+pulse = spritesheet(("intro_1","pulse"), 8, zlevel = 1, frames = 95)
+climbers = spritesheet(("intro_1","climbers"), 7, zlevel = 10, frames = 95)
+dudeontop = spritesheet(("intro_1","dudeontop"), 3, zlevel = 11, frames = 95)
+moon = spritesheet(("intro_1","moon"), 8, zlevel = 0.5, frames = 1)
+#### Unpack this in run_animation_curses. Should look like this: run_animation_curses([insert curses window here],*intro_1) 
+intro_1 = (fire,fire2,fire3,skyscraper,building1,heli,citybg,pulse, climbers, dudeontop, moon)
+
+##### Intro part 2 
+intro_male_eyes = spritesheet(("intro_2","intro_male_eyes"), 1, zlevel = 5, frames = 38)
+intro_male_body = spritesheet(("intro_2","intro_male_body"), 11, zlevel = 3, frames = 1)
+intro_male_hair = spritesheet(("intro_2","intro_male_hair"), 2, zlevel = 10, frames = 1)
+bed = spritesheet(("intro_2","bed"), 5, zlevel = 0.1, frames = 1)
+pillow = spritesheet(("intro_2","pillow"), 12, zlevel = 0.2, frames = 1)
+#### Unpack this in run_animation_curses
+intro_2 = (intro_male_body,intro_male_hair,intro_male_eyes, bed, pillow, general_bg,outline)
+
+##### Tutorial Room
+bed = spritesheet(("room_tutorial", "bed"), 5, zlevel = 12, frames = 1)
+bg = spritesheet(("room_tutorial", "bg"), 3, zlevel = 1, frames = 1)
+cobweb = spritesheet(("room_tutorial", "cobweb"), 1, zlevel = 10, frames = 1)
+misc = spritesheet(("room_tutorial", "misc"), 1, zlevel = 20, frames = 1)
+scratches = spritesheet(("room_tutorial", "scratches"), 1, zlevel = 11, frames = 1)
+wood = spritesheet(("room_tutorial", "wood"), 2, zlevel = 15, frames = 1)
+#### Unpack in print_stillshot_curses. Should look like print_stillshot_curses([0,0,0,0,0,0,0],display_win, *room_tutorial). The list thing is dumb, I'll rework it.
+room_tutorial = (outline,bed, bg, cobweb, misc, scratches, wood)
+
+##### Cutscene 1
+zombie_1 = spritesheet(("cutscene_1", "zombie"), 7, zlevel = 12, frames = 58)
+body_1 = spritesheet(("cutscene_1", "body_1"), 11, zlevel = 3, frames = 1)
+red_elements_1 = spritesheet(("cutscene_1", "red"), 8, zlevel = 15, frames = 58)
+red_elements_2 = spritesheet(("cutscene_1", "red2"), 8, zlevel = 4, frames = 1)
+cutscene_1 = (outline,zombie_1, red_elements_1, general_bg, body_1, red_elements_2)
+
+#####
+holder1 = spritesheet(("placeholder",), 8, zlevel = 4, frames = 6, dx = 22, dy = 2)
+# holder2 = spritesheet(("placeholder",), 7, zlevel = 5, frames = 6, dx = 24, dy = 5)
+# holder3 = spritesheet(("placeholder",), 7, zlevel = 4, frames = 6, dx = 27, dy = 8)
+
+cutscene_1_1 = (outline,zombie_1, red_elements_1, general_bg, body_1, red_elements_2, randomobj)
--- modularanimation/combat.py
+++ modularanimation/combat.py
@@ -1,7 +1,11 @@
 import curses
+import random
+import copy
 from ani_sprites import *
 
-from pprint import pprint # debugging purposes
+curses.initscr()
+# curses_setcolors()
+
 # from animator import *
 
 # What do I want to achieve here?
@@ -35,19 +39,34 @@ from pprint import pprint # debugging purposes
 # 3nd step: Handle the battle state going from stillshot, to something moving, and back to stillshot. Calling standstill and attack.
 
 class creature():
-    def __init__(self, path, creature_type, creature_name, creature_level, creature_zlevel, dx_change, dy_change):
+    def __init__(self, path,creature_number, creature_type, color, creature_level = 1, hp = 20, creature_zlevel = 1, dx_change = 0, dy_change = 0):
         self.path = path
+        self.creature_number = 0
         self.creature_type = creature_type
-        self.creature_name = creature_name
         self.creature_level = creature_level
-        self.z = creature_zlevel
-        self.hp = 100
-        self.basedmg = 3
-        self.spritesheet = spritesheet(self.path, 11, zlevel = creature_zlevel, frames = 6, dx = dx_change, dy = dy_change )
+        self.zlevel = creature_zlevel # Priority it takes when printing the animation
+        self.hp = hp
+        self.color = color
+        self.basedmg = 3 #Placeholder. Should use self.set_base_damage
+        self.dx_change = dx_change
+        self.dy_change = dy_change
+        self.frames = 6
+        self.spritesheet = spritesheet(self.path, self.color, zlevel = self.zlevel, frames = self.frames, dx = self.dx_change, dy = self.dy_change)
         self.frameslist = self.spritesheet.frameslist
-        # self.frames = 6
         self.frames_attack = self.spritesheet.frameslist
         self.frames_standstill = self.spritesheet.frameslist[0]
+        
+    def set_base_damage(self):
+        #### Base damage should be set based off the type of the creature
+        pass
+        
+    def set_original_spritesheet(self): # Brings the spritesheet back to its original amount of frames
+        del self.spritesheet
+        self.spritesheet = spritesheet(self.path, self.color, zlevel = self.zlevel, frames = self.frames, dx = self.dx_change, dy = self.dy_change)
+        self.frameslist = self.spritesheet.frameslist
+        self.frames_attack = self.spritesheet.frameslist
+        self.frames_standstill = self.spritesheet.frameslist[0]
+        # self.spritesheet.frameslist = self.gather_framelist()
 
     def gather_framelist(self): ### WIP here the name of the creature will be appended below every sprite. Currently, it's just a copy of the original gather_framelist. No use yet.
         frameslist = []
@@ -61,55 +80,143 @@ class creature():
 
 
 class initiate_combat():
-    def __init__(self, curses_window, *enemies):
-        self.creatures_dict = {}
+    def __init__(self, curses_window, enemies):
+        self.creatures_dict = {} ### creatures in the current fight stored here. Protagonist is the first key "protag"
+        self.info_dict = {} # dictionary to store info enemy : info spritesheet pair
         self.enemies = enemies
-        self.curses_window = curses_window # hold for now
-        self.enemies_alive = len(self.enemies)
+        self.curses_window = curses_window
+        self.enemies_alive = len(self.creatures_dict.keys())-1
+        self.escape = False # If true, breaks from battle loop
         self.place_creatures()
-        # self.draw_on_window()
-    
-    def place_creatures(self):
-        dx_change = 33
-        dy_change = 3
-        name_counter = 1
-        counter = 1
-        z = 6
+        # self.info_details() # obsolete for now 
+        self.current_weapon = "" # NEEDS WORK: Pull the item of the players from the backend?. This will then be used to set the corresponding spriteshet of the protagonist (WIP)
+        self.stillstate = self.set_stillstate() # Returns tuple which is used in print_stillshot_curses
+        self.animation = "" # the animation which will take place is stored here as a tuple
         
-        ### Could use a rudimentary leveling up system for the protagonist
+    def refresh_sprites(self): # sets the spritesheets back to their original state
+        for i in self.creatures_dict:
+            self.creatures_dict[i].set_original_spritesheet()
+        # for i in self.info_dict: # WIP, UPDATES INFO AFTER EVERY ATTACK
+            # self.info_dict[i]
+        self.stillstate = self.set_stillstate()    
+    
+    def refresh_info(self, spritesheet, info):
+        pass
+        
+    def change_protag_sprites(self): #WIP. This will fire when the protag changes weapons.
+        pass
+        
+    def blank_out_frames(self, spritesheet_to_modify): # NEW
+        for index, value in enumerate(spritesheet_to_modify):
+            if index%2 == 1:
+                newframe = ""
+                for i in value:
+                    if i == "\n":
+                        newframe += "\n"
+                    else:
+                        newframe += " "
+                spritesheet_to_modify[index] = newframe        
+    
+    def freeze_all(self, i): # Extends the first frame to the other frames in the same spritesheet
+        firstframe = self.creatures_dict[i].spritesheet.frameslist[0]
+        for index, value in enumerate(self.creatures_dict[i].spritesheet.frameslist):
+            self.creatures_dict[i].spritesheet.frameslist[index] = firstframe 
+            
+    def set_new_sprites(self, attacker, attacked): # Makes changes to sprites and alters self.animation, the tuple which will be used when calling run_animation_curses
+        self.refresh_sprites()
+        if attacker.creature_type == "protag":
+            for i in self.creatures_dict:
+                if i != "protag":
+                    self.freeze_all(i) 
+        
+        if attacker.creature_type != "protag":
+            firstframe = self.creatures_dict["protag"].spritesheet.frameslist[0]
+            for index, value in enumerate(self.creatures_dict["protag"].spritesheet.frameslist):
+                self.creatures_dict["protag"].spritesheet.frameslist[index] = firstframe  
+            for i in self.creatures_dict:
+                if self.creatures_dict[i] != attacker:
+                    self.freeze_all(i)   
+            
+        self.blank_out_frames(attacked.spritesheet.frameslist)
+                        
+        tuple_enemies = tuple([self.creatures_dict[i] for i in self.creatures_dict])
+        self.curses_window.clear()
+        self.animation = (self.curses_window, general_bg, *tuple_enemies)
+        
+    def set_stillstate(self):
+        ########### gotta fix this
+        return ([0 for i in range(20)], self.curses_window, *self.spritesheets_tuple())
+    
+    def user_update(self, user_input): #### this should also 
+        if user_input == "escape":
+            self.escape = True
+            return
+        #### Here, the user input must be in the format "attack_1", "attack_2".. etc
+        if user_input[:-2] == "attack":
+            damage = 5 # Placeholder. This can go through a random damage generator that's based on the player's base attack stat
+            protag = self.creatures_dict["protag"]
+            enemy = self.creatures_dict["Enemy_{0}".format(user_input[-1:])]
+            enemy.hp -= damage
+            self.set_new_sprites(protag, enemy)
+
+    def zombie_update(self): ### NEEDS WORK. Should attack character based on enemy's basedmg
+        #### Placeholder
+        protag = self.creatures_dict["protag"]
+        enemies = [creature for creature in self.creatures_dict.values() if creature.creature_type != "protag"]
+        enemy = random.choice(enemies)
+        protag.hp -= enemy.basedmg # should be modified so it's random to some degree
+        print(protag.hp)
+        self.set_new_sprites(enemy, protag)
+        # self.refresh_sprites()
+        #### FUNCTION TO UPDATE ANIMATION TUPLE HERE
+        
+    def place_creatures(self):
+        dx_change = 30
+        dy_change = -4
+        name_counter = 1
+        enemy_number = 1
+        counter = 1 # to keep track of when to change dx, dy to different values 
+        z = 6       
+        protag_level = 1 # placeholder
+        ### Could use a rudimentary leveling up system for the protag
         ### such as basic attack and hp going up by a given percentage upon level up
-        ### creature(path, creature_type, creature_name, creature_level, creature_zlevel, dx_change, dy_change)
-        protag = creature(("placeholder",),"protag", "You", 1, 1, 2, 15) #level can be replaced with protagonist level variable
+        
+        ### creature(path, creature_type, creature_name, creature_level, hp, creature_zlevel, dx_change, dy_change)
+        protag = creature(("placeholder",),0,"protag", 13, protag_level, 50, 20, 2, 9) #level can be replaced with protag level variable
         self.creatures_dict["protag"] = protag
         for i in self.enemies:
-            # enemy = spritesheet(("placeholder",), 3, zlevel = z, frames = 2)
             enemy_type = i[0]
             enemy_level = i[1]
-            enemy_name = "Zombie_{0}".format(name_counter)
-            enemy = creature(("placeholder",),enemy_type, enemy_name, enemy_level, z, dx_change, dy_change)
-            self.creatures_dict["Enemy_{0}".format(name_counter)] = enemy
+            hp = 25 #pull enemy HP from backend
             dx_change += 10
             dy_change += 8
             counter += 1
-            name_counter += 1
+            # print(self.enemies[name_counter-1][1])
+            enemy_level = self.enemies[name_counter-1][1] #grabs second item from tuple
+            enemy = creature(("placeholder",),enemy_number,enemy_type, 7, enemy_level,hp, z, dx_change, dy_change)
+            self.creatures_dict["Enemy_{0}".format(name_counter)] = enemy
+            info = spritesheet(("placeholder",),14, zlevel = z+0.1, frames = 6, dx = dx_change, dy = dy_change+5, infolist = [enemy_type, enemy_number,enemy_level,hp])
+            self.info_dict[enemy] = info
             if counter == 4:
                 counter = 1
                 dx_change -= 10
-                dy_change = 3
+                dy_change = -6
             z += 1
+            enemy_number += 1
+            name_counter += 1
     
-    def draw_on_window(self):
-        # need everything as a tuple
-        print(len(self.creatures_dict))
-        creatures_to_add = []
+    def spritesheets_tuple(self):
+        # need everything as a tuple so I can unpack it
+        spritesheets_to_add = [general_bg]
         for i in self.creatures_dict:
-            creatures_to_add.append(self.creatures_dict[i].spritesheet)
-        # print(creatures_to_add)
-        creatures_to_add = tuple(creatures_to_add)
-        print(creatures_to_add)
-        # frames_to_print = [0 for i in range(len(creatures_to_add))]
-        # return (frames_to_print,self.curses_window, *creatures_to_add)
-        return creatures_to_add
+            spritesheets_to_add.append(self.creatures_dict[i].spritesheet)
+            # print(self.creatures_dict[i], "THIS IS IT ")
+            # print(self.info_dict)
+            # spritesheets_to_add.append(self.info_dict[self.creatures_dict[i]].spritesheet) # NOT IMPLEMENTED YET
+        for i in self.info_dict:
+            spritesheets_to_add.append(self.info_dict[i])
+        spritesheets_to_add = tuple(spritesheets_to_add)
+        return spritesheets_to_add
         
         
     
