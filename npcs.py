1080410e44a868706ebe482d7419b2b8f4c3cb3f
Sam Tyler <samtyler905@gmail.com>
2024-10-26 00:24:08 +0100

    Added NPC class and example npc dictionary

---
+++ npcs.py
@@ -0,0 +1,16 @@
+class NPC:
+    def __init__(self, npc_dict : {}):
+        self.id = npc_dict["id"]
+        self.name = npc_dict["name"]
+        self.lines = []
+
+
+
+# Define NPC dictionaries here
+
+test_npc = {
+    "id" : "test_npc",
+    "name" : "Testing NPC",
+    "lines" : ["Hi", "Hello", "Hey there", "Howdy"],
+    "escape_items" : None
+}
