cc24a8d9b5fee376ae45c562a00a93e5a26c287a
alxm9 <alxmoc96@gmail.com>
2024-10-26 12:45:49 +0100

    Code clean-up. Documentation added.

--- animator.py
+++ animator.py
@@ -19,31 +19,23 @@ def adjust_to_max(list_framelists, max_frame): # adjusts to max frame
             list.extend([list[-1]]*(max_frame - len(list)))
     return list_framelists
 
-####### is having dx and dy in every spritesheet the right approach?
-####### I suppose during a battle, I could have a loop create an "enemy" : obj dictionary and then just delete it.
-####### after I'm done with it.
-####### yeah that could work. Also I can have extend_frame function exist in every object as a method so every instance of an enemy gets placed based on the increments of dx and dy
-####### maybe encapsulate the battle in an object?
-
-#### CURSES #############################################################################
-#### WIP
-def run_animation_curses(win, *args):
-    max_frame = max(args, key=attrgetter('frames')).frames
-    list_framelists = [i.frameslist for i in args]
-    # for i in args:
-        # if i.nonstandard != 0:
-    list_framelists = adjust_to_max(list_framelists, max_frame)
-    for framelist in zip(*list_framelists): # * unpacks list_framelists into n different lists (objs)
+def preliminary_adjustments(args): # Grabs max frame, duplicates last frame.
+    max_frame = max(args, key=attrgetter('frames')).frames # Gets the highest value of the spritesheet with most frames in args.
+    list_framelists = [i.frameslist for i in args] # A list containing len(args) amount of lists, with each containing the frames of its respective spritesheet.
+    list_framelists = adjust_to_max(list_framelists, max_frame) # Duplicates the final frame of every framelist to the amount of max_frame.
+    return list_framelists
+    
+def run_animation_curses(win, *args): # Runs the animation, the parameters are the curses window and ani_sprites tuple.
+    list_framelists = preliminary_adjustments(args) # Duplicates final frames if needed.
+    for framelist in zip(*list_framelists): # * unpacks list_framelists into n different lists
         print_frame_curses(framelist,win,args)
-        win.move(0,0)
+        win.move(0,0) # Moves printing cursor back to position 0,0
         win.refresh()
-        time.sleep(0.05)
+        time.sleep(0.05) # Frame rate of the animation
 
 def run_animation_curses_pad(win, pad_args, ui_lock, *args): #First arg should be edge/bg.
-    max_frame = max(args, key=attrgetter('frames')).frames
-    list_framelists = [i.frameslist for i in args]
-    list_framelists = adjust_to_max(list_framelists, max_frame)
-    for framelist in zip(*list_framelists): # * unpacks list_framelists into n different lists (objs)
+    list_framelists = preliminary_adjustments(args) # Duplicates final frames if needed.
+    for framelist in zip(*list_framelists): # * unpacks list_framelists into n different lists
         ui_lock.acquire()
         print_frame_curses(framelist,win,args)
         win.move(0,0)
@@ -51,10 +43,8 @@ def run_animation_curses_pad(win, pad_args, ui_lock, *args): #First arg should b
         ui_lock.release()
         time.sleep(0.05)
         
-def print_stillshot_curses(framenumlist,win,*args): # [1,4], dude, backpillars
-    max_frame = max(args, key=attrgetter('frames')).frames
-    list_framelists = [i.frameslist for i in args]
-    list_framelists = adjust_to_max(list_framelists, max_frame)
+def print_stillshot_curses(framenumlist,win,*args):
+    list_framelists = preliminary_adjustments(args)
     frames_to_print = []
     counter = 0
     for i in range(len(args)):
@@ -64,9 +54,7 @@ def print_stillshot_curses(framenumlist,win,*args): # [1,4], dude, backpillars
         print_frame_curses(framelist,win,args)
 
 def print_stillshot_curses_pad(win, pad_args,ui_lock,*args): # [1,4], dude, backpillars
-    max_frame = max(args, key=attrgetter('frames')).frames
-    list_framelists = [i.frameslist for i in args]
-    list_framelists = adjust_to_max(list_framelists, max_frame)
+    list_framelists = preliminary_adjustments(args) # Duplicates final frames if needed.
     frames_to_print = []
     counter = 0
     for i in range(len(args)):
@@ -78,20 +66,20 @@ def print_stillshot_curses_pad(win, pad_args,ui_lock,*args): # [1,4], dude, back
         win.refresh(pad_args[0], pad_args[1], pad_args[2], pad_args[3], pad_args[4], pad_args[5])
         ui_lock.release()
         
-def print_frame_curses(framelist,win,args):
+def print_frame_curses(framelist,win,args): # Prints frame on the screen
     for chars in zip(*framelist): # framelist unpacked into char number of different characters
-        z_char_color = {}
-        compare = []
+        z_char_color = {} # Dictionary that stores zlevel: [ascii character to print, color]
+        compare = [] # zlevel of every ascii char
         if all(" " in t for t in chars): # if all 3 chars are " " 
-            win.addstr(" ")
+            win.addstr(" ") # prints empty space 
             continue
-        for i in range(len(chars)): # zlevel: [char, color]
+        for i in range(len(chars)): # zlevel of current arg: [current ascii char, color of current arg]
             z_char_color[args[i].zlevel] = [chars[i], args[i].color]
         for i in z_char_color:
-            if z_char_color[i][0] != " ":
+            if z_char_color[i][0] != " ": # Appending empty space to the comparison list is pointless.
                 compare.append(i)
         to_print=compare[0]
-        for i in compare:
+        for i in compare: # assigns the highest zlevel to to_print, and it is then used in z_char_color to access the char and color to print.
             if i > to_print:
                 to_print = i
         win.addstr(z_char_color[to_print][0], curses.color_pair(z_char_color[to_print][1]))
@@ -105,7 +93,7 @@ def main(stdscr): ### TESTING PURPOSES ## Uncomment whichever path you need in t
     # curses.start_color()
     curses_setcolors() # imported from ani_sprites.py
     # print(cutscene_1_1, "WORKING")
-    # run_animation_curses(display_win,*cutscene_1_1)
+    # run_animation_curses(display_win,*cutscene_1)
     # zombies = (("zombie", 2),("zombie", 1), ("zombie", 1),("zombie", 1),("zombie", 1),("zombie", 1),("zombie", 1),("zombie", 1)) #this tuple will be what's returned from the random battle generator
     #fight = initiate_combat(display_win,*zombies)
     # print(fight.draw_on_window(), "NOT WORKING")
@@ -120,7 +108,7 @@ def main(stdscr): ### TESTING PURPOSES ## Uncomment whichever path you need in t
     display_win.refresh()
     display_win.getch()
     display_win.clear()
-    # print_stillshot_curses([0,0,0,0,0,0,0],display_win, *room_tutorial) ## ([frames to print for each arg], curses window, unlimited args..)
+    # print_stillshot_curses([0,0,0,0,0,0,0,0,0],display_win, *room_tutorial) ## ([frames to print for each arg], curses window, unlimited args..)
     display_win.refresh()
     display_win.getch()
 
